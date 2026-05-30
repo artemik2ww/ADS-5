b6b9f50ef415610f0ae4bb6af9931bb0f48186e3
Артём <71376415+artemik2ww@users.noreply.github.com>
2026-05-30 17:22:43 +0300

    b125

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,40 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    enum { kMaxSize = size };
+    T data[kMaxSize];
+    int count;
+
+ public:
+    TStack() : count(0) {}
+
+    bool isEmpty() const {
+        return count == 0;
+    }
+
+    bool isFull() const {
+        return count == kMaxSize;
+    }
+
+    void push(const T& value) {
+        if (count < kMaxSize) {
+            data[count++] = value;
+        }
+    }
+
+    void pop() {
+        if (count > 0) {
+            count--;
+        }
+    }
+
+    T get() const {
+        if (count > 0) {
+            return data[count - 1];
+        }
+        return T();
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
